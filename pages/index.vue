e1b055207463833d219c70f867176d6903c09df5
0x0 <ghstoreinvesting@gmail.com>
2018-07-20 18:40:23 +0800

    Add display of user current location

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,7 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-toolbar
-      dense
-      floating
-      class='search-bar'
-    >
-      <v-text-field
-        hide-details
-        prepend-icon="search"
-        single-line
-      ></v-text-field>
+    <v-toolbar dense floating class='search-bar'>
+      <v-text-field hide-details prepend-icon="search" single-line></v-text-field>
 
       <v-btn icon>
         <v-icon>my_location</v-icon>
@@ -19,47 +11,62 @@
         <v-icon>more_vert</v-icon>
       </v-btn>
     </v-toolbar>
-      <v-card height="calc(100vh - 64px)" width="100vw">
-         
-        <gmap-map
-          :center="{lat:-31.980293, lng:115.817935}"
-          :zoom="17"
-          map-type-id="terrain"
-          style="width: 100%; height: 100%;"
-        >
-          <GmapMarker
-            :key="index"
-            v-for="(m, index) in accessibleMarkers"
-            :position="m.position"
-            :icon="m.icon"
-          />
-        </gmap-map>
-      </v-card>
+    <v-card height="calc(100vh - 64px)" width="100vw">
+
+      <gmap-map :center="{lat:-31.980293, lng:115.817935}" :zoom="17" map-type-id="terrain" style="width: 100%; height: 100%;">
+        <GmapMarker :key="index" v-for="(m, index) in accessibleMarkers" :position="m.position" :icon="m.icon" />
+
+        <GmapMarker :position="currentLocation" icon="/person-pin.svg">
+          <gmap-info-window>
+            <span class='black--text'>You are here</span>"
+          </gmap-info-window>
+        </GmapMarker>
+
+
+
+      </gmap-map>
+    </v-card>
   </v-layout>
 </template>
 
 <script>
+  export default {
+    mounted() {
+      this.geolocation()
+    },
 
-export default {
-  data () {
-    let wheelChairIcon = '/wheel-chair-icon.gif'
-    let publicTransIcon =  '/public-transport-icon.gif'
-    let toiletIcon = '/toilet-icon.gif'
-    let accessibleRampsIcon = '/accessible-ramps-icon.gif'
-    let liftIcon = '/lift-icon.gif'
-    return {
-      accessibleMarkers: [
-        {
+    data() {
+      let wheelChairIcon = '/wheel-chair-icon.gif'
+      let publicTransIcon = '/public-transport-icon.gif'
+      let toiletIcon = '/toilet-icon.gif'
+      let accessibleRampsIcon = '/accessible-ramps-icon.gif'
+      let liftIcon = '/lift-icon.png'
+      return {
+        accessibleMarkers: [{
           icon: wheelChairIcon,
           position: {
             lat: -31.980293,
-            lng:115.817935
+            lng: 115.817935
           }
-        }
-      ]
+        }],
+        currentLocation: {
+          lat: 0,
+          lng: 0
+        },
+        currentLocationText: 'You are here'
+      }
+    },
+
+    methods: {
+      geolocation: function () {
+        navigator.geolocation.getCurrentPosition(position => {
+          this.currentLocation.lat = position.coords.latitude
+          this.currentLocation.lng = position.coords.longitude
+        })
+      }
     }
   }
-}
+
 </script>
 
 <style scoped>
@@ -69,4 +76,5 @@ export default {
     top: 100px;
     z-index: 9999;
   }
+
 </style>
