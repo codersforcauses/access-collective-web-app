472c61ccc0ca52f8873c9faff1a19ec60f1d656f
0x0 <ghstoreinvesting@gmail.com>
2018-07-23 21:17:54 +0800

    Add contact page

--- components/NavDraw.vue
+++ components/NavDraw.vue
@@ -1,6 +1,27 @@
 <template>
   <v-navigation-drawer :mini-variant.sync="miniVariant" v-model='$store.state.navDraw' fixed app>
+     <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            Application
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
 
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-tile to="/contact">
+        <v-list-tile-action>
+          <v-icon>question_answer</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          Contact Us
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
