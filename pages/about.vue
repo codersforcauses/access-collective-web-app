461fbd9a7b15f03a4db18322cc34942896bc8bfe
0x0 <ghstoreinvesting@gmail.com>
2018-07-29 00:32:34 +0800

    Add about page

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-card>
+		<v-card-media :src="require('~/assets/CFC_Logo.png')" height="500px">
+		</v-card-media>
+
+		<v-card-title primary-title>
+			<div>
+				<h3 class="headline">About</h3>
+				<div>This web app has been proudly brought to you by <a href="https://codersforcauses.org">Coders for Causes</a> in collaboration with Guild Access Department</div>	
+			</div>
+					
+		</v-card-title>
+	</v-card>
+</template>
