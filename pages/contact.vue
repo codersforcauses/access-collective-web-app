d013a7f59b7b4865531c31aa1082748f471a0204
0x0 <ghstoreinvesting@gmail.com>
2018-08-03 14:34:37 +0800

    Use google forms instead

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,32 +1,7 @@
 <template>
-  <div>
-    <div class="typeform-widget" data-url="https://davidyu2.typeform.com/to/CtLXPW" style="width: 100%; height: 500px;"></div>
-    <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by
-      <a href="https://admin.typeform.com/signup?utm_campaign=CtLXPW&utm_source=typeform.com-12366537-Basic&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN"
-        style="color: #999" target="_blank">Typeform</a>
-    </div>
-  </div>
+<v-layout align-center justify-center row fill-height class="mt-3">
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfeoazPyEefydUf5xbtM4b9eHuq3UZU_LQ356whCLz8nTIFSw/viewform?embedded=true" width="700" height="700" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+</v-layout>
+
+ 
 </template>
-
-<script>
-  export default {
-    created() {
-      (function () {
-        var qs, js, q, s, d = document,
-          gi = d.getElementById,
-          ce = d.createElement,
-          gt = d.getElementsByTagName,
-          id = "typef_orm",
-          b = "https://embed.typeform.com/";
-        if (!gi.call(d, id)) {
-          js = ce.call(d, "script");
-          js.id = id;
-          js.src = b + "embed.js";
-          q = gt.call(d, "script")[0];
-          q.parentNode.insertBefore(js, q)
-        }
-      })()
-    }
-  }
-
-</script>
