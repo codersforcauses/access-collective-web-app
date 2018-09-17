a736a0dfa542c978a998d0ce3b6f73c6942225c4
0x0 <ghstoreinvesting@gmail.com>
2018-09-17 18:27:16 +0800

    Compile

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-`git add .`
-`git commit -m "Compile"`
-`git subtree push --prefix dist origin gh-pages`
+git add .
+git commit -m "Compile"
+git subtree push --prefix dist origin gh-pages
